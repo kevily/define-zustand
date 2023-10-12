3e582bd69d8f4cef8e21ffa3cdb69eaf24388d46
1k <bug_zero@163.com>
2023-10-12 11:01:18 +0800

    feat: action.getState add getter type

--- packages/define-zustand/src/types.ts
+++ packages/define-zustand/src/types.ts
@@ -1,5 +1,10 @@
 import { StoreApi as ZStoreApi } from 'zustand'
 
+export type defGetterStateType<S> = Record<string, (state: S) => any>
+export type ExtraGetterState<S, G extends defGetterStateType<S>> = {
+    [key in keyof G]: ReturnType<G[key]>
+}
+
 export type StoreApi<S> = Omit<ZStoreApi<S>, 'setState'> & {
     setState: (updater: ((state: S) => void) | Partial<S>, replace?: boolean) => void
 }
@@ -8,19 +13,19 @@ export interface insideActionsType<S> {
     setState: StoreApi<S>['setState']
     subscribe: StoreApi<S>['subscribe']
 }
-export type actionsType<S, OptionAction, InsideActions = unknown> = (
-    getState: () => S,
+export type actionsType<
+    S,
+    G extends defGetterStateType<S>,
+    OptionAction,
+    InsideActions = unknown
+> = (
+    getState: () => S & ExtraGetterState<S, G>,
     actions: Omit<insideActionsType<S>, 'subscribe'> & InsideActions,
     store: StoreApi<S>
 ) => OptionAction
 
-export type defGetterStateType<S> = Record<string, (state: S) => any>
-
-export type ExtraGetterState<S, G extends defGetterStateType<S>> = {
-    [key in keyof G]: ReturnType<G[key]>
-}
-export interface optionsType<S, G, OptionAction> {
+export interface optionsType<S, G extends defGetterStateType<S>, OptionAction> {
     state: () => S
     getter: G
-    actions: actionsType<S, OptionAction>
+    actions: actionsType<S, G, OptionAction>
 }
